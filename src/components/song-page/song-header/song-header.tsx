d96b84112c469e9463b4fc57c2710cdcc8345810
ellaklara <ellaklara.westerlund@gmail.com>
2020-04-04 23:29:17 +0200

    Update styling

--- src/components/song-page/song-header/song-header.tsx
+++ src/components/song-page/song-header/song-header.tsx
@@ -4,6 +4,7 @@ import LoadingImg from '../../loading-img/loading-img';
 import PlusButton from '../../../assets/icons/plus-circle-outline.svg'
 import { GeniusSong } from '../../../model/genius/geniusTypes';
 import Dialog from "../../dialog/dialog";
+import songPage from '../song-page';
 
 const SongHeader: FC<{song: GeniusSong, songInLibrary: Function, library: GeniusSong[], toggleLibrary: Function}> = (props) => {
 
@@ -18,26 +19,28 @@ const SongHeader: FC<{song: GeniusSong, songInLibrary: Function, library: Genius
     }
 
     return (
-        <div className='song-header'>
+        <div className='song-header' style={{backgroundImage: 'url('+props.song.primary_artist.header_image_url+')'}}>
             <Dialog open={openDialog} onAccept={props.toggleLibrary} toggleScopeDialog={() => setOpenDialog(!openDialog)}>
                 Are you sure you want to remove this song from your library?<br/>
                 You will lose all changes made to the lyrics.
             </Dialog>
-            <div className='song-image'>
-                <LoadingImg alt={props.song.title} src={props.song.song_art_image_url}/>
+            <div className='black-overlay'>
+                <div className='song-image'>
+                    <LoadingImg alt={props.song.title} src={props.song.song_art_image_url}/>
+                </div>
+                <div className='song-details'> 
+                    <div className='song-title'>
+                        {props.song.title}
+                        <span className='add-song'>
+                            <img alt={'add/remove song'} src={PlusButton} className={props.songInLibrary() ? 'added' : ''} style={{height: '1em'}} 
+                            onClick={toggleDialog}/>
+                        </span>
+                    </div>
+                    <div className='song-artist'>
+                        {props.song.primary_artist.name}
+                    </div>
+                </div> 
             </div>
-            <div className='song-details'> 
-                <div className='song-title'>
-                    {props.song.title}
-                    <span className='add-song'>
-                        <img alt={'add/remove song'} src={PlusButton} className={props.songInLibrary() ? 'added' : ''} style={{height: '1em'}} 
-                        onClick={toggleDialog}/>
-                    </span>
-                </div>
-                <div className='song-artist'>
-                    {props.song.primary_artist.name}
-                </div>
-            </div> 
         </div>
     );
 }
